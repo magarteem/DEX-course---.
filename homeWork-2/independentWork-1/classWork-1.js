7a588c3cfbe7039d488bd5b02ddfefb4711ffd18
magarteem <45105983+magarteem@users.noreply.github.com>
2021-11-12 05:04:33 +0200

    add link codesandbox.io

--- homeWork-2/independentWork-1/classWork-1.js
+++ homeWork-2/independentWork-1/classWork-1.js
@@ -1,3 +1,6 @@
+// codesandbox.io
+// https://codesandbox.io/s/thirsty-kate-lnzb3?file=/src/workHere/workHere.js
+
 export const sum = (f, s) => {
   //TODO: Должны складываться только строки и числа
   // Постарайтесь сложить как можно больше пар и обойти NaN случаи
