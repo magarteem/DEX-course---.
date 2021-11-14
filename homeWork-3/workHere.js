22af60b987c65403e7a91508c21ab88845522ca7
magarteem <45105983+magarteem@users.noreply.github.com>
2021-11-15 01:07:47 +0200

    add homeork-3

---
+++ homeWork-3/workHere.js
@@ -0,0 +1,133 @@
+export const processCartData = (cartData) => {
+  //TODO: Нужно добавить поле discount(oldPrice - price)
+  // убрать поле oldPrice
+  //let discount =
+  cartData.map((e) => {
+    if (e.oldPrice - e.price >= 0) {
+      return {
+        ...e,
+        ...(e.discount = e.oldPrice ? e.oldPrice - e.price : null),
+        ...delete e?.oldPrice,
+      };
+    }
+    return {
+      ...e,
+      ...delete e?.oldPrice,
+    };
+  });
+  return cartData;
+  //return discount;
+};
+
+export const makeCartItemCopy = (cartItem) => {
+  //TODO: сделать копию элемента "Пицца с анчоусами"
+  // После увеличить кол-во добавленного ингредиента
+  const newElem = cartItem.find((x) => x.id === "0");
+
+  console.log(newElem);
+  console.log(cartItem);
+  return cartItem;
+};
+
+//export const makeCartItemCopy = (cartItem) => {
+//  //TODO: сделать копию элемента "Пицца с анчоусами"
+//  // После увеличить кол-во добавленного ингредиента
+//  const newElem = cartItem.find((x) => x.id === "0");
+//  const newIngr = {
+//    ...newElem,
+//    addedIngredients: [
+//      {
+//        ...newElem.addedIngredients[0],
+//        count: newElem.addedIngredients[0].count + 1,
+//      },
+//    ],
+//  };
+//  return newIngr;
+//};
+
+export const calcSum = (cartData) => {
+  //TODO: посчитать суммы по типам товаров и их цены
+  let showCountPrice = {
+    total: { count: 0, sum: 0 },
+    water: { count: 0, sum: 0 },
+    pizza: { count: 0, sum: 0 },
+    other: { count: 0, sum: 0 },
+  };
+  cartData.forEach((e) => {
+    for (const key in showCountPrice) {
+      if (key === e.type) {
+        showCountPrice[key].count++;
+        showCountPrice[key].sum += e.price;
+      }
+    }
+  });
+  return showCountPrice;
+};
+
+export const getCartItemsByDate = (cartData, date) => {
+  //date = "2021-10-28T20:55:15.220Z"; // пришедшая дата
+  //TODO: выбрать покупки сделанные за выбранную дату
+  const getDate = new Date(date).toLocaleDateString(); // пришло
+  date &&
+    (cartData = cartData.filter(
+      (el) => new Date(el.date).toLocaleDateString() === getDate && el
+    ));
+  return cartData;
+};
+
+export const repeatOrder = (cartData, date) => {
+  //date = "2021-10-28T20:55:15.220Z"; // пришедшая дата
+  //TODO: нужно повторить заказ за выбранную дату
+  // дублиовать соответствующие элементы
+  // поставить в начало спиcка
+  // дату текущую
+  // поменять id на уникальный
+
+  const getDate = new Date(date).toLocaleDateString(); // пришло
+  cartData.map((el) => {
+    cartData = [...cartData];
+    if (new Date(el.date).toLocaleDateString() === getDate) {
+      return [
+        ...cartData,
+        cartData.unshift({
+          ...el,
+          id: cartData.length,
+          date: new Date(Date.now()),
+        }),
+      ];
+    }
+  });
+  return cartData;
+};
+
+export const addItem = (cartData, item) => {
+  //TODO: увеличить кол-во товара в полученном элементе
+  let find = cartData.map((x) => {
+    return { ...x, count: x.id === item.id && x.count++ };
+  });
+  return cartData;
+};
+
+export const checkPromo = (cartData) => {
+  //TODO: нужно проверить корзина подходит под правила промоакции
+  // проверить что суммарно в корзине больше 1000р
+  // что есть пункт больше чем на 500р
+  // что нет скидочных товаров
+
+  let price = 0;
+  let maxPrice = [];
+  let discount = false;
+
+  cartData.forEach((e) => {
+    price += e.price * e.count;
+    maxPrice.push(e.price * e.count);
+    discount = e.discount ? false : true;
+  });
+  const calcMaxPrice = maxPrice.find((summ) => summ > 500);
+
+  return {
+    total: price > 1000,
+    oneBigPosition: calcMaxPrice > 500,
+    notDiscounted: discount,
+  };
+};
