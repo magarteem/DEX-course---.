b6ed6f77339610d938e9b3e3224ae2662ad3492f
magarteem <45105983+magarteem@users.noreply.github.com>
2021-11-23 13:18:23 +0200

    setInterval setTimeout

--- homeWork-1/independentWork-5/class.js
+++ homeWork-1/independentWork-5/class.js
@@ -158,6 +158,8 @@ console.log(personStudent.validatePassword());
 console.log(personStudent.getMetodStudents());
 console.log(personStudent.useParentMetod());
 
+//========= instanceof=================
+console.log(personStudent instanceof Student);
 //============= GET SET ==========================
 
 class People {
@@ -182,33 +184,30 @@ console.log(peop);
 console.log(peop.peopleName);
 console.log(peop.privatMetod());
 
+//========= instanceof=================
+console.log(peop instanceof People);
 //=====================================
 //=====================================
-//=====================================
+
+// переписать из функции в класс
 class Clock {
   constructor({ template }) {
     this.template = template;
   }
-
   render() {
     let date = new Date();
-
     let hours = date.getHours();
     if (hours < 10) hours = "0" + hours;
-
     let mins = date.getMinutes();
     if (mins < 10) mins = "0" + mins;
-
     let secs = date.getSeconds();
     if (secs < 10) secs = "0" + secs;
-
     let output = this.template
       .replace("h", hours)
       .replace("m", mins)
       .replace("s", secs);
     console.log(output);
   }
-
   stop() {
     clearInterval(this.timer);
   }
@@ -217,6 +216,5 @@ class Clock {
     this.timer = setInterval(() => this.render(), 1000);
   }
 }
-
 let clock = new Clock({ template: "h:m:s" });
-clock.start();
+//clock.start();
---
+++ homeWork-1/independentWork-5/setTimeout.js
@@ -0,0 +1,22 @@
+// вывести число каждую секунду, начиная от from и заканчивая to.
+//Используя setInterval и setTimeout.
+
+const from = 2;
+const to = 6;
+
+const printNumbers = (from, to) => {
+  const setInt = setInterval(() => {
+    from != to + 1 ? console.log(from) : clearInterval(setInt);
+    from++;
+  }, 1000);
+};
+printNumbers(from, to);
+
+function printNumbers1(from, to) {
+  let t = setTimeout(function go() {
+    console.log(from);
+    from++;
+    from < to + 1 && setTimeout(go, 1000);
+  }, 1000);
+}
+printNumbers1(from, to);
