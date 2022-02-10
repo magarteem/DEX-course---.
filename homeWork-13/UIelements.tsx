a891896fa9fc2ef7c0a2064b085e3b4404f5d6e4
magarteem <fmf311@mail.ru>
2022-02-10 23:55:21 +0200

    add style css_1

--- homeWork-13/UIelements.tsx
+++ homeWork-13/UIelements.tsx
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 import s from "./uielements.module.css";
 import logo from "../../assets/icons/logo.webp";
 import userIcon from "../../assets/icons/userIcon.webp";
+import pencil from "../../assets/icons/pencil.svg";
+import deleteImg from "../../assets/icons/delete.svg";
+import teamLogo from "../../assets/icons/teamLogo.webp";
+import playerPhoto from "../../assets/icons/playerPhoto.webp";
 
 export const UIelements = () => {
   return (
@@ -115,8 +119,45 @@ export const UIelements = () => {
         <h2>Card Team</h2>
 
         <div className={s.cardTeam}>
-          <title>Teams / Denver Nuggets</title>
-          <div className={s.teamInfo}>1</div>
+          <div className={s.teamTitle}>
+            <span>
+              Teams <span className={s.teamTitle__text}>/</span> Denver Nuggets
+            </span>
+            <div className={s.iconEdit}>
+              <img src={pencil} alt="edit" />
+              <img src={deleteImg} alt="delete" />
+            </div>
+          </div>
+
+          <div className={s.teamInfo}>
+            <div className={s.teamLogo}>
+              <img src={teamLogo} alt="teamLogo" />
+            </div>
+            <div className={s.teamText}>
+              <h1>Denver Nuggets</h1>
+              {/*<div>*/}
+              <div className={s.left50}>
+                <div>
+                  <h4>Year of foundation</h4>
+                  <span>1976</span>
+                </div>
+
+                <div>
+                  <h4>Conference</h4>
+                  <span>Western</span>
+                </div>
+
+                <div>
+                  <h4>Division</h4>
+                  <span>Northwestern</span>
+                </div>
+              </div>
+              {/*<div className={s.rig50}>
+                  
+                </div>*/}
+              {/*</div>*/}
+            </div>
+          </div>
         </div>
       </section>
 
